06fd6b62b14d8d64f3227a608dfb22a950d7a599
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-11-13 20:42:18 -0800

    prettified and added prettifier git hook w/ husky

--- test/getPeriodsFromDayCount.test.ts
+++ test/getPeriodsFromDayCount.test.ts
@@ -40,56 +40,54 @@ describe('getPeriodsFromDayCount', () => {
         period: 1
       },
       */
-    expect(await getPeriodsFromDayCount(new Date(2021, 10, 15), 1)).toEqual(
+    expect(await getPeriodsFromDayCount(new Date(2021, 10, 15), 1)).toEqual([
       [
-        [
-          {
-            start: new Date('2021-11-15 8:40 am PST'),
-            end: new Date('2021-11-15 9:25 am PST'),
-            period: 1
-          },
-          {
-            start: new Date('2021-11-15 9:32 am PST'),
-            end: new Date('2021-11-15 10:22 am PST'),
-            period: 2
-          },
-          {
-            start: new Date('2021-11-15 10:22 am PST'),
-            end: new Date('2021-11-15 10:32 am PST'),
-            period: 'Brunch'
-          },
-          {
-            start: new Date('2021-11-15 10:39 am PST'),
-            end: new Date('2021-11-15 11:24 am PST'),
-            period: 3
-          },
-          {
-            start: new Date('2021-11-15 11:31 am PST'),
-            end: new Date('2021-11-15 12:16 pm PST'),
-            period: 4
-          },
-          {
-            start: new Date('2021-11-15 12:16 pm PST'),
-            end: new Date('2021-11-15 1:01 pm PST'),
-            period: 'Lunch'
-          },
-          {
-            start: new Date('2021-11-15 1:08 pm PST'),
-            end: new Date('2021-11-15 1:53 pm PST'),
-            period: 5
-          },
-          {
-            start: new Date('2021-11-15 2:00 pm PST'),
-            end: new Date('2021-11-15 2:45 pm PST'),
-            period: 6
-          },
-          {
-            start: new Date('2021-11-15 2:52 pm PST'),
-            end: new Date('2021-11-15 3:37 pm PST'),
-            period: 7
-          }
-        ]
+        {
+          start: new Date('2021-11-15 8:40 am PST'),
+          end: new Date('2021-11-15 9:25 am PST'),
+          period: 1
+        },
+        {
+          start: new Date('2021-11-15 9:32 am PST'),
+          end: new Date('2021-11-15 10:22 am PST'),
+          period: 2
+        },
+        {
+          start: new Date('2021-11-15 10:22 am PST'),
+          end: new Date('2021-11-15 10:32 am PST'),
+          period: 'Brunch'
+        },
+        {
+          start: new Date('2021-11-15 10:39 am PST'),
+          end: new Date('2021-11-15 11:24 am PST'),
+          period: 3
+        },
+        {
+          start: new Date('2021-11-15 11:31 am PST'),
+          end: new Date('2021-11-15 12:16 pm PST'),
+          period: 4
+        },
+        {
+          start: new Date('2021-11-15 12:16 pm PST'),
+          end: new Date('2021-11-15 1:01 pm PST'),
+          period: 'Lunch'
+        },
+        {
+          start: new Date('2021-11-15 1:08 pm PST'),
+          end: new Date('2021-11-15 1:53 pm PST'),
+          period: 5
+        },
+        {
+          start: new Date('2021-11-15 2:00 pm PST'),
+          end: new Date('2021-11-15 2:45 pm PST'),
+          period: 6
+        },
+        {
+          start: new Date('2021-11-15 2:52 pm PST'),
+          end: new Date('2021-11-15 3:37 pm PST'),
+          period: 7
+        }
       ]
-    )
+    ])
   })
 })
